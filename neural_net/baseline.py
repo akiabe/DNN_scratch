8bbf68f192dcafe669e01a1770567fbc4d3a8a91
Akifumi Abe <akiabe0831@gmail.com>
2021-02-15 07:37:52 +0900

    update

--- neural_net/baseline.py
+++ neural_net/baseline.py
@@ -82,4 +82,10 @@ x = np.array([1.0, 0.5])
 y = forward(network, x)
 print(y, y.shape)
 
-a = np.array
+a = np.array([0.3, 2.9, 4.0])
+exp_a = np.exp(a)
+print(exp_a)
+sum_exp_a = np.sum(exp_a)
+print(sum_exp_a)
+y = exp_a / sum_exp_a
+print(y)
