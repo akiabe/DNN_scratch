8b4225bbe732015c1683313b11d1f6b9b3c96a6c
Akifumi Abe <akiabe0831@gmail.com>
2021-02-15 06:35:03 +0900

    update

---
+++ neural_net/baseline.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x = np.array([-1.0, 1.0, 2.0])
+print(sigmoid(x))
+
+
+
+def relu(x):
+    return np.maximum(0, 4)
+
+print(relu(x))
+
+
+
+X = np.array([1, 2])
+W = np.array([[1, 3, 5], [2, 4, 6]])
+Y = np.dot(X, W)
+print(X.shape)
+print(W.shape)
+print(Y)
+
+
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+A1 = np.dot(X, W1) + B1
+print(X.shape)
+print(W1.shape)
+print(B1.shape)
+print(A1)
+
+Z1 = sigmoid(A1)
+print(Z1)
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+A2 = np.dot(Z1, W2) + B2
+print(A2, A2.shape)
+
+Z2 = sigmoid(A2)
+print(Z2, Z2.shape)
+
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+A3 = np.dot(Z2, W3) + B3
+print(A3, A3.shape)
+
+
+
+def init_network():
+    network = {}
+    network["W1"] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network["b1"] = np.array([0.1, 0.2, 0.3])
+    network["W2"] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network["b2"] = np.array([0.1, 0.2])
+    network["W3"] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network["b3"] = np.array([0.1, 0.2])
+
+    return network
+
+def forward(network, x):
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = a3
+
+    return y
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y, y.shape)
+
+a = np.array
