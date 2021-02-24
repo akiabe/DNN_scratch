d88e52134d80a48d235070e1848599ac2e441501
Akifumi Abe <akiabe0831@gmail.com>
2021-02-25 07:02:05 +0900

    update

--- neural_net/cost.py
+++ neural_net/cost.py
@@ -50,10 +50,40 @@ def cross_entropy_error_lbl(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
+
     batch_size = y.shape[0]
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+    for idx in range(x.size):
+        tmp_val = x[idx]
 
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+
+    return grad
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    return x
+
+def function(x):
+    return x[0]**2 + x[1]**2
+
+init_x = np.array([-3.0, 4.0])
+x = gradient_descent(function, init_x=init_x, lr=0.1, step_num=100)
+print(x)
 
 
 
